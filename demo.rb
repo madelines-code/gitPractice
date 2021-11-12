8b5a8e3d312a82c84f548592e81ab2160cf4bd9a
Madeline Armstrong <Madeline@Sammy-Dewalds-MacBook-Air.local>
2021-11-12 11:13:20 -0700

    added dummy data

--- demo.rb
+++ demo.rb
@@ -1,2 +1,13 @@
 puts "hello world"
 puts 1 + 1 = 2
+
+# setup (do this for each project)
+# git init to create repo
+# create a repo on github
+# add the remote link
+
+# basic flow
+# make change sin your code
+# add(stage) those changes git add . or git add filename
+# conmmit those changes git commit -m'description of what you did'
+# push up to github
